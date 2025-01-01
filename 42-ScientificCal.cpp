e4791fb57804acf87090bc614628ea26cd3ced1a
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-01-01 11:31:16 +0530

    Add files via upload

---
+++ 42-ScientificCal.cpp
@@ -0,0 +1,15 @@
+
+/*
+Create 2 classes
+    1. Simple calculator :- Takes input of 2 numbers using utility function and performs +, - , *, / and display the result using another function.
+
+    2. Scientific Calculator :- Take input of 2 numbers using a utility function and performs any four scientific operation of your choice and display the result using another function.
+
+
+    Create another class HybridCalculator and inherit it using these 2 classes:
+    Q1. What type of inheritance are you using?
+    Q2. Which mode of inheritance are you using?
+    Q3. create an object of HybridCalculator and display results of simple and scientific calculator.
+    Q4. How is code reusability implemented?
+
+*/
